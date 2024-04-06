6b9d3b02ba1ed1f2deb9a7837ac9ec240e7344cd
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-06 18:45:31 -0500

    Resolving rubocop issues

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,21 +1,12 @@
-require "json"
-require "open-uri"
+# frozen_string_literal: true
 
+# app/controllers/games_controller.rb
 class GamesController < ApplicationController
   def new
     @letters = ('A'..'Z').to_a.sample(10)
   end
 
   def score
-    characters = params[:word].upcase.chars
-    # @missing = false
-    # characters.each do |c|
-    #   @missing = @letters.none?(c)
-    #   break if @missing
-    # end
-    # unless missing
-    #   url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
-    #   @results = JSON.parse(URI.open(url).read)
-    # end
+    # characters = params[:word].upcase.chars
   end
 end

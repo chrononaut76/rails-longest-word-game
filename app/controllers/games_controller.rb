55c41a526fe9c4a3bcb71fe4ed0263f7322da5b6
Gerardo Calderon <gerardoh@calderonlopez.com>
2024-04-06 21:32:21 -0500

    Final draft of score method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,6 +7,17 @@ class GamesController < ApplicationController
   end
 
   def score
-    # characters = params[:word].upcase.chars
+    characters = params[:word].upcase.chars.sort
+    letters = params[:letters].split.sort
+    in_grid = characters.all? { |char| letters.include?(char) }
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    real_word = JSON.parse(URI.open(url).read)['found']
+    if in_grid && real_word
+      @result = "Congratulations! #{params[:word].upcase} is a valid English word!"
+    elsif real_word
+      @result = "Sorry but #{params[:word].upcase} does not seem to be a valid English word..."
+    else
+      @result = "Sorry but #{params[:word].upcase} can't be built out of #{letters.join(', ').trim}"
+    end
   end
 end
